53e8896bd2d6b48696fe19077c4195cda68917ef
ArmanndeepKaur <armanndeep.22@gmail.com>
2023-02-10 23:29:26 -0500

    added task B

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "funcs.h"
+
+
+int main() 
+{
+  print_interval(3,19);
+  std::cout<<"\n";
+
+  print_interval(-100, -83);
+  std::cout<<"\n";
+
+  print_interval(-7, 11);
+  std::cout<<"\n";
+
+  print_interval(999, 1100);
+  std::cout<<"\n";
+  
+}
